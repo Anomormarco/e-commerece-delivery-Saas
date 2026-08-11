6e9fd9c0524e9c0d4161920232f6632054ad29bf
myngaa_0711 <Anomormarco@gmail.com>
2026-08-11 19:49:04 +0800

    Force courier app redeploy marker

--- client/src/apps/courier/main.tsx
+++ client/src/apps/courier/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "../../styles.css";
 import { CourierApp } from "./CourierApp";
 
+document.documentElement.dataset.deliverhubApp = "courier-profile-header-v4";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CourierApp />
