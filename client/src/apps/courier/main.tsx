ea93665160be0184cc9cd2bb1b1970c1c5cc1cdb
myngaa_0711 <Anomormarco@gmail.com>
2026-08-11 20:42:00 +0800

    Trigger employee deploy with project settings

--- client/src/apps/courier/main.tsx
+++ client/src/apps/courier/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "../../styles.css";
 import { CourierApp } from "./CourierApp";
 
-document.documentElement.dataset.deliverhubApp = "courier-session-flow-v8";
+document.documentElement.dataset.deliverhubApp = "courier-session-flow-v9";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
